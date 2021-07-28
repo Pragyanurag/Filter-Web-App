1cccf32145e67ae15a08218a2cf3af9d8cd0de54
PragyanuragDas <85403639+Pragyanurag@users.noreply.github.com>
2021-07-28 17:57:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+}
+
+function draw() {
+
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png');
+}
